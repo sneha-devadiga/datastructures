34b1afedcabd6bf4e9413e342c3ada8ccb621fa7
Sneha Devadiga <snehadevadiga14@gmail.com>
2024-07-28 14:49:01 +0530

    linkedList - reverse linked list

---
+++ LinkedList/src/main/java/ReverseLinkedList.java
@@ -0,0 +1,22 @@
+import utils.Node;
+
+/**
+ * Time Complexity : O(n)
+ * Space Complexity : O(1)
+ */
+public class ReverseLinkedList {
+    public Node reverse(Node head) {
+        Node pointer = head;
+
+        Node prev = null, temp = null;
+
+        while (pointer != null) {
+            temp = pointer.next;
+            pointer.next = prev;
+            prev = pointer;
+            pointer = temp;
+        }
+
+        return prev;
+    }
+}
---
+++ LinkedList/src/main/java/utils/Node.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class Node {
+    public int data;
+    public Node next;
+    public Node() {}
+    public Node(int data) {
+        this.data = data;
+    }
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
