9269e2f08ae6dc4c06a51bcc0e548c1af28f65d2
Sneha Devadiga <snehadevadiga14@gmail.com>
2024-08-01 19:01:21 +0530

    linkedList - Update to Reverse Linked List In Given Range

--- LinkedList/src/main/java/ReverseLinkedListInGivenRange.java
+++ LinkedList/src/main/java/ReverseLinkedListInGivenRange.java
@@ -14,7 +14,8 @@ public class ReverseLinkedListInGivenRange {
         for (int i = 0; i < end - start; i++) {
             Node temp = tail.next; // holds the neighboring value of first node
             tail.next = temp.next; // breaks the link between neighboring nodes and assigns first node's
-            // neighbor as second node's neighbor
+            // neighbor as second node's neighbor.
+            // Since tail is not modified which is still start node -> it's next will eventually point to end's next
             temp.next = prev.next; // second nodes neighbor becomes first node
             // imagine reversing 2 nodes viz 1 -> 2 expected output 1 <- 2.
             // Only links are reversed in reversal of LL
@@ -22,5 +23,12 @@ public class ReverseLinkedListInGivenRange {
         }
 
         return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5)))));
+        ReverseLinkedListInGivenRange rr = new ReverseLinkedListInGivenRange();
+        Node ans = rr.reverse(head, 2, 4);
+        System.out.println();
     }
 }
