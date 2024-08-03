8577cca0f97f926453bfe09113f4741b759dc494
Sneha Devadiga <snehadevadiga14@gmail.com>
2024-08-03 21:27:31 +0530

    linkedList - RemoveNthNode from End and LinkedListCycleII

---
+++ LinkedList/src/main/java/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,32 @@
+import utils.Node;
+
+/**
+ * Approach - 2 pointers (slow and fast)
+ * Time Complexity : O(n)
+ * Space Complexity : O(1)
+ */
+public class RemoveNthNodeFromEndOfList {
+
+    /**
+     * modified the head in place
+     */
+    public void delete(Node head, int n) {
+        Node fast = head;
+        Node slow = head;
+
+        for (int i = 0; i < n; i++) fast = fast.next;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+    }
+
+    public static void main(String[] args) {
+        Node _1 = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5)))));
+        RemoveNthNodeFromEndOfList re = new RemoveNthNodeFromEndOfList();
+        re.delete(_1, 2);
+        System.out.println();
+
+    }
+}
